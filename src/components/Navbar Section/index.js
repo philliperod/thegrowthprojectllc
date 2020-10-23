04d002fd49473b6f8e28237dc3357e598d275d6b
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-22 19:51:34 -0500

    added styling for navbar elements; imported react router, icons, styled components

--- src/components/Navbar Section/index.js
+++ src/components/Navbar Section/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  Hamburger,
+  NavMenu,
+  NavItem,
+  NavLinks
+} from './NavbarElements';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo>logo</NavLogo>
+          <NavLogo to="/">logo</NavLogo>
+          <Hamburger>
+            <FaBars />
+          </Hamburger>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="home">Home</NavLinks>
+            </NavItem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
