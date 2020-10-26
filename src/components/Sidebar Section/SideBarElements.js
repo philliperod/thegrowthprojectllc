3e3c96744022f333ed5848d0772f31b5ba8c4a74
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-26 09:56:38 -0400

    added hero and sidebar section; will need to add more components and styling

---
+++ src/components/Sidebar Section/SideBarElements.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FaTimes } from 'react-icons/fa';
+import { Link as LinkR } from 'react-router-dom';
+import { Link as LinkS } from 'react-scroll';
+
+export const SideBarContainer = styled.aside`
+  position: fixed;
+  z-index: 20;
+  width: 100%;
+  height: 100%;
+  background: #90b4ce;
+  display: grid;
+  align-items: center;
+`;
---
+++ src/components/Sidebar Section/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <SideBarContainer>
+        <SideBarWrapper>
+            <SideBarMenu>
+                <SideBarLink>Home</SideBarLink>
+            </SideBarMenu>
+        </SideBarWrapper>
+    </SideBarContainer>);
+
+export default SideBar;
